b747c52d62a2c7568ceeef63840d19dab0ea727c
Jose Daniel <jose.grijalba04@gmail.com>
2025-09-21 14:40:25 -0500

    fix

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,23 +7,23 @@ export const routes: Routes = [
     loadComponent: () => import('./modules/home/home.component').then(c => c.HomeComponent)
   },
   {
+    path: 'auth',
+    loadComponent: () => import('./modules/auth-profile/auth-profile.component').then(c => c.AuthProfileComponent),
+    children: [
+      {
+        path: 'login',
+        loadComponent: () => import('./modules/auth-profile/login/login.component').then(c => c.LoginComponent)
+      },
+      {
+        path: 'register',
+        loadComponent: () => import('./modules/auth-profile/register/register.component').then(c => c.RegisterComponent)
+      }
+    ]
+  },
+  {
     path: '',
     component: LayoutComponent,
     children: [
-      {
-        path: 'auth',
-        loadComponent: () => import('./modules/auth-profile/auth-profile.component').then(c => c.AuthProfileComponent),
-        children: [
-          {
-            path: 'login',
-            loadComponent: () => import('./modules/auth-profile/login/login.component').then(c => c.LoginComponent)
-          },
-          {
-            path: 'register',
-            loadComponent: () => import('./modules/auth-profile/register/register.component').then(c => c.RegisterComponent)
-          }
-        ]
-      },
       {
         path: 'dashboard',
         loadChildren: () => import('./modules/dashboard/dashboard.module').then(m => m.DashboardModule),
